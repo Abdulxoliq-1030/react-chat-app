faa20bd5153cbcd5cb1e3fb5818ee82616633043
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-07 09:03:03 +0500

    real time chat change typescript and next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React, { useState } from 'react';
+import io from "socket.io-client"
+import "./page.css"
+import Chat from '@/components/chat';
+interface PageProps { }
+
+const socket = io("http://localhost:3001")
+
+const Page: React.FC<PageProps> = () => {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return <div className='App'>
+    {!showChat ? (
+      <div className="joinChatContainer">
+        <h3>Join A Chat</h3>
+        <input
+          type="text"
+          placeholder="John..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Room ID..."
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+        />
+        <button onClick={joinRoom}>Join A Room</button>
+      </div>
+    ) : <Chat socket={socket} username={username} room={room} />}
+  </div>
+}
+
+export default Page;
